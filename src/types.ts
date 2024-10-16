32376d5140db7c624b033aa1a518c3f793bd0a37
juan-rueda-appxcale <juan.rueda@appxcale.com>
2024-10-16 16:12:46 -0500

    feat: add style property for default calendar day item
    Style prop that enables users to define a specific look for unselected and enable days

--- src/types.ts
+++ src/types.ts
@@ -40,6 +40,7 @@ export type CalendarThemeProps = {
   weekDaysContainerStyle?: ViewStyle;
   weekDaysTextStyle?: TextStyle;
   calendarTextStyle?: TextStyle;
+  calendarItemStyle?: ViewStyle;
   selectedTextStyle?: TextStyle;
   selectedItemColor?: string;
   timePickerContainerStyle?: ViewStyle;
