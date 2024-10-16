610a2b9b787e8307bf1ad8f00e2024ee10d5637d
juan-rueda-appxcale <juan.rueda@appxcale.com>
2024-10-16 15:06:34 -0500

    style: remove test code in example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -209,7 +209,6 @@ export default function App() {
               // eslint-disable-next-line react-native/no-inline-styles
               todayContainerStyle={{
                 borderWidth: 1,
-                borderColor: '#999',
               }}
             />
             <View style={styles.footer}>
